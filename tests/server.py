6a4ab678363a9bd3c311723fc161f36e86263c87
Edu Pagotto <edupagotto@gmail.com>
2025-11-10 20:05:10 -0300

    teste de carga de modulo

---
+++ tests/server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+Created on 20251110
+Update on 20251110
+@author: Eduardo Pagotto
+'''
+
+import os
+import sys
+sys.path.append(os.path.join(os.getcwd(), './src'))
+
+from sjsonrpc.asy import RPC_Responser
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
